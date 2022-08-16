5d4b0771c2f088a909095a223561cd25543fc2b0
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-08-16 19:06:51 +0530

    Data Segregation Completed

---
+++ index.js
@@ -0,0 +1,33 @@
+const express=require('express');
+
+const app=express();
+const PORT=process.env.PORT ||8080
+var path = require('path');
+ 
+app.use(express.static("assets"));
+
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+ 
+app.get('/twitter-tab',(req,res)=>{
+  res.sendFile(path.join(__dirname + '/twitter-tab.html'));
+})
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+ 
