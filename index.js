23ff6c50f8fde78a5f4d5cc73b39da428f1146f6
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-08-19 09:56:06 +0530

    this

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 
 const app=express();
-const PORT=process.env.PORT ||8080
+const PORT= process.env.PORT ||8080
 var path = require('path');
  
 app.use(express.static("assets"));
